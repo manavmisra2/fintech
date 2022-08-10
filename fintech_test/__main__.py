c520b9091a50f51b536c955ddcb048c92f955313
manavmisra2 <manavmisra2@gmail.com>
2022-08-11 01:00:34 +0530

    first commit

---
+++ fintech_test/__main__.py
@@ -0,0 +1,31 @@
+import site;
+import os
+os.system("pip install jupyterlab_templates")
+os.system("jupyter labextension install jupyterlab_templates")
+os.system("jupyter serverextension enable --py jupyterlab_templates")
+def main():
+    print("Running Jupyter platform!") 
+    out=site.getsitepackages()
+    str_out = ""     
+    for ele in out: 
+        str_out += ele 
+    filename =  os.path.join(str_out, "finailab", "jupyter_notebook_config.py")
+    filename2 = filename.replace(os.sep, '/')    
+    os.system("jupyter lab --config=" + filename2)
+   
+
+if __name__ == "__main__":
+    main()
+
+
+def listToString(s): 
+    
+    # initialize an empty string
+    str1 = "" 
+    
+    # traverse in the string  
+    for ele in s: 
+        str1 += ele  
+    
+    # return string  
+    return str1 
